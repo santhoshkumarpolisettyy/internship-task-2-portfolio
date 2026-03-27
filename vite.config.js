2291d02824e662f5750d13a08cd4e2cd1a38420e
Santhosh Kumar <santhoshkumarpolisettyy.com>
2026-03-27 17:38:42 +0530

    fixer multipage build issue

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,30 @@
 import { defineConfig } from 'vite'
 import nunjucks from 'nunjucks'
+import fs from 'fs'
+import path from 'path'
+
+function renderNunjucks() {
+  return {
+    name: 'nunjucks-transform',
+
+    transformIndexHtml(html, ctx) {
+      const filePath = ctx.filename
+      const template = fs.readFileSync(filePath, 'utf-8')
+      return nunjucks.renderString(template)
+    }
+  }
+}
 
 export default defineConfig({
   build: {
     outDir: 'dist',
-
     rollupOptions: {
       input: {
-        main: 'index.html',
-        services: 'services.html',
-        contact: 'contact.html'
+        main: path.resolve(__dirname, 'index.html'),
+        services: path.resolve(__dirname, 'services.html'),
+        contact: path.resolve(__dirname, 'contact.html')
       }
     }
   },
-
-  plugins: [
-    {
-      name: 'nunjucks-transform',
-      transformIndexHtml(html) {
-        return nunjucks.renderString(html)
-      }
-    }
-  ]
+  plugins: [renderNunjucks()]
 })
